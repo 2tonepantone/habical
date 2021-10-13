06274caf7a2a285fe10e02ad95fe88578820f586
Kyle Bailey <kylealexbailey@gmail.com>
2021-10-13 16:11:46 +0900

    fix indentation

--- db/migrate/20211013070609_add_columns_to_users.rb
+++ db/migrate/20211013070609_add_columns_to_users.rb
@@ -1,6 +1,6 @@
 class AddColumnsToUsers < ActiveRecord::Migration[6.1]
   def change
-        add_column :users, :name, :string
+    add_column :users, :name, :string
     add_column :users, :access_token, :string
     add_column :users, :expires_at, :datetime
     add_column :users, :refresh_token, :string
