6ab8e217d9d4d5d53ac990dfd4756150f6e7dcfa
Kyle Bailey <kylealexbailey@gmail.com>
2021-10-18 16:01:24 +0900

    refactor and rmeove redundancies

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,8 +5,8 @@ class TasksController < ApplicationController
   CALENDAR_ID = 'primary'.freeze
 
   def index
-    @busy_times = get_busy_times
-    @events = get_calendar_events
+    @busy_times = fetch_busy_times
+    @events = fetch_calendar_events
   end
 
   # GET /tasks/new
@@ -15,7 +15,7 @@ class TasksController < ApplicationController
   end
 
   def create
-    client = get_google_calendar_client(current_user)
+    client = fetch_google_calendar_client
     task = params[:task]
     event = get_event(task)
     client.insert_event('primary', event)
@@ -25,7 +25,7 @@ class TasksController < ApplicationController
 
   private
 
-  def get_google_calendar_client(current_user)
+  def fetch_google_calendar_client
     client = Google::Apis::CalendarV3::CalendarService.new
     return unless current_user.present? && current_user.access_token.present? && current_user.refresh_token.present?
 
@@ -58,47 +58,45 @@ class TasksController < ApplicationController
 
   def get_event(task)
     attendees = task[:members].split(',').map { |t| { email: t.strip } }
-    event = Google::Apis::CalendarV3::Event.new({
-                                                  summary: task[:title],
-                                                  location: '800 Howard St., San Francisco, CA 94103',
-                                                  description: task[:description],
-                                                  start: {
-                                                    date_time: Time.new(task['start_date(1i)'], task['start_date(2i)'], task['start_date(3i)'], task['start_date(4i)'],
-                                                                        task['start_date(5i)']).to_datetime.rfc3339,
-                                                    time_zone: 'Asia/Kolkata'
-                                                    # date_time: '2019-09-07T09:00:00-07:00',
-                                                    # time_zone: 'Asia/Kolkata',
-                                                  },
-                                                  end: {
-                                                    date_time: Time.new(task['end_date(1i)'], task['end_date(2i)'], task['end_date(3i)'], task['end_date(4i)'],
-                                                                        task['end_date(5i)']).to_datetime.rfc3339,
-                                                    time_zone: 'Asia/Kolkata'
-                                                  },
-                                                  attendees: attendees,
-                                                  reminders: {
-                                                    use_default: false,
-                                                    overrides: [
-                                                      Google::Apis::CalendarV3::EventReminder.new(
-                                                        reminder_method: 'popup', minutes: 10
-                                                      ),
-                                                      Google::Apis::CalendarV3::EventReminder.new(
-                                                        reminder_method: 'email', minutes: 20
-                                                      )
-                                                    ]
-                                                  },
-                                                  notification_settings: {
-                                                    notifications: [
-                                                      { type: 'event_creation', method: 'email' },
-                                                      { type: 'event_change', method: 'email' },
-                                                      { type: 'event_cancellation', method: 'email' },
-                                                      { type: 'event_response', method: 'email' }
-                                                    ]
-                                                  }, 'primary': true
-                                                })
+    Google::Apis::CalendarV3::Event.new({
+                                          summary: task[:title],
+                                          location: '800 Howard St., San Francisco, CA 94103',
+                                          description: task[:description],
+                                          start: {
+                                            date_time: Time.new(task['start_date(1i)'], task['start_date(2i)'], task['start_date(3i)'], task['start_date(4i)'],
+                                                                task['start_date(5i)']).to_datetime.rfc3339,
+                                            time_zone: 'Asia/Kolkata'
+                                          },
+                                          end: {
+                                            date_time: Time.new(task['end_date(1i)'], task['end_date(2i)'], task['end_date(3i)'], task['end_date(4i)'],
+                                                                task['end_date(5i)']).to_datetime.rfc3339,
+                                            time_zone: 'Asia/Kolkata'
+                                          },
+                                          attendees: attendees,
+                                          reminders: {
+                                            use_default: false,
+                                            overrides: [
+                                              Google::Apis::CalendarV3::EventReminder.new(
+                                                reminder_method: 'popup', minutes: 10
+                                              ),
+                                              Google::Apis::CalendarV3::EventReminder.new(
+                                                reminder_method: 'email', minutes: 20
+                                              )
+                                            ]
+                                          },
+                                          notification_settings: {
+                                            notifications: [
+                                              { type: 'event_creation', method: 'email' },
+                                              { type: 'event_change', method: 'email' },
+                                              { type: 'event_cancellation', method: 'email' },
+                                              { type: 'event_response', method: 'email' }
+                                            ]
+                                          }, 'primary': true
+                                        })
   end
 
-  def get_busy_times
-    client = get_google_calendar_client(current_user)
+  def fetch_busy_times
+    client = fetch_google_calendar_client
     body = Google::Apis::CalendarV3::FreeBusyRequest.new
     body.items = ["id": 'primary']
     body.time_min = Time.now.iso8601
@@ -110,8 +108,8 @@ class TasksController < ApplicationController
     @busy_times = @response.calendars['primary'].busy
   end
 
-  def get_calendar_events
-    client = get_google_calendar_client(current_user)
+  def fetch_calendar_events
+    client = fetch_google_calendar_client
     @events = client.list_events(CALENDAR_ID,
                                  max_results: 10,
                                  single_events: true,
