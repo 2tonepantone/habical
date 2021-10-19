85d9904c5e63b8f5d7564e699a60521eb9761b90
Kyle Bailey <kylealexbailey@gmail.com>
2021-10-19 18:31:19 +0900

    migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_13_071926) do
+ActiveRecord::Schema.define(version: 2021_10_19_092131) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "busy_blocks", force: :cascade do |t|
+    t.datetime "start_date"
+    t.datetime "end_date"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
