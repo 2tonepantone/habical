2cb87408b40722f7aef1db23a3348b6935e0aab9
Kyle Bailey <kylealexbailey@gmail.com>
2021-10-19 19:16:32 +0900

    implement proper user schedule initialization

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -7,6 +7,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       @user.access_token = auth.credentials.token
       @user.expires_at = auth.credentials.expires_at
       @user.refresh_token = auth.credentials.refresh_token
+      @user.schedule = Schedule.new unless @user.schedule
       @user.save!
       sign_in(@user)
       redirect_to tasks_path
