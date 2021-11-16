59a3edfbfcf678a27b69d9e7adc9638ab6d1a77f
Kyle Bailey <kylealexbailey@gmail.com>
2021-11-16 11:16:57 +0900

    remove unnecessary code

--- app/javascript/components/CalendarComponent.js
+++ app/javascript/components/CalendarComponent.js
@@ -1,15 +1,5 @@
 import React from "react";
-import PropTypes from 'prop-types'
 import Calendar from "@ericz1803/react-google-calendar";
-
-//put your google calendar api key here
-// const API_KEY = "";
-
-//replace calendar id with one you want to test
-
-// let calendars = [
-//   { calendarId: "09opmkrjova8h5k5k46fedmo88@group.calendar.google.com" }
-// ];
 
 const CalendarComponent = (props) => (
     <div className="Calendar">
