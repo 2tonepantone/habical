82ba50520682fb6bcc796ae24455c7f9398d8207
Kyle Bailey <kylealexbailey@gmail.com>
2022-03-11 16:51:00 +0900

    remove timeGridWeek plugin to make calendar more responsive

--- app/javascript/components/WeekCalendar.js
+++ app/javascript/components/WeekCalendar.js
@@ -7,7 +7,7 @@ import timeGridPlugin from '@fullcalendar/timegrid'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import sampleEvents from '../../assets/data/sampleEvents'
 
-const WeekCalendar = ({ events, sample }) =>  {
+const WeekCalendar = ({ events, user_present }) =>  {
   const [formattedEvents, setFormattedEvents] = useState(null)
 
   const formatEvents = (events) => {
@@ -19,31 +19,25 @@ const WeekCalendar = ({ events, sample }) =>  {
   }
 
   useEffect(() => {
-    setFormattedEvents(formatEvents(sample ? sampleEvents : events))
+    setFormattedEvents(formatEvents(user_present ? events : sampleEvents))
   }, [])
 
   return (
     <FullCalendar
-      plugins={[dayGridPlugin, timeGridPlugin, listPlugin, bootstrap5Plugin]}
+      plugins={[dayGridPlugin, listPlugin, bootstrap5Plugin]}
       initialView='listWeek'
       headerToolbar={{
         left: 'prev,next today',
         center: 'title',
-        right: 'dayGridMonth,timeGridWeek,listWeek,listDay'
+        right: 'dayGridMonth,listWeek,listDay'
       }}
       buttonText={{
         today: 'Today',
         dayGridMonth: 'Month',
-        timeGridWeek: 'Week',
-        listWeek: 'Week List',
-        listDay: 'Day List'
+        listWeek: 'Week',
+        listDay: 'Day'
       }}
       navLinks='true'
-      slotDuration='00:10'
-      slotMinTime='09:00'
-      slotMaxTime='21:00'
-      allDaySlot={false}
-      nowIndicator='true'
       themeSystem='bootstrap5'
       events={formattedEvents}
     />
