e3c7102b1f2f2adfccb2e1ec736ecc5bda75197b
Kyle Bailey <kylealexbailey@gmail.com>
2022-03-11 12:30:30 +0900

    add sample calendar events

--- app/assets/data/sampleEvents.js
+++ app/assets/data/sampleEvents.js
@@ -2,45 +2,105 @@ import moment from "moment"
 
 const sampleEvents =  [
   {
-    title: "Festival in HH",
-    start: moment().add(1, 'days').format(),
+    title: "Japanese Class",
+    start: moment().subtract(1, 'days').add(2, 'hours').format(),
+    end: moment().subtract(1, 'days').add(3, 'hours').format(),
+  },
+  {
+    title: "Meetup w/ Ryan",
+    start: moment().add(4, 'days').subtract(2, 'hours').format(),
     end: moment().add(4, 'days').add(3, 'hours').format(),
   },
   {
-    title: "Party in HH",
-    start: moment().subtract(2, 'days').format(),
-    end: moment().subtract(2, 'days').add(3, 'hours').format(),
+    title: "Jill's Birthday",
+    start: moment().add(8, 'days').add(2, 'hours').format(),
+    end: moment().add(8, 'days').add(5, 'hours').format(),
   },
   {
-    title: "Party in SH",
-    start: moment().format(),
+    title: "Family Video Chat",
+    start: moment().subtract(4, 'days').add(2, 'hours').format(),
+    end: moment().subtract(4, 'days').add(3, 'hours').format(),
+  },
+  {
+    title: "Water Plants",
+    start: moment().subtract(2, 'days').add(1, 'hours').format(),
+    end: moment().subtract(2, 'days').add(2, 'hours').format(),
+  },
+  {
+    title: "Shelby's Birthday Party",
+    start: moment().add(2, 'days').add(3, 'hours').format(),
+    end: moment().add(2, 'days').add(5, 'hours').format(),
+  },
+  {
+    title: "Car Service",
+    start: moment().add(14, 'days').subtract(2, 'hours').format(),
+    end: moment().add(14, 'days').add(4, 'hours').format(),
+  },
+  {
+    title: "Trash Day",
+    start: moment().add(6, 'days').subtract(7, 'hours').format(),
+    end: moment().add(6, 'days').subtract(6, 'hours').format(),
+  },
+  {
+    title: "Chat w/ Marle",
+    start: moment().add(7, 'days').add(1, 'hours').format(),
+    end: moment().add(7, 'days').add(2, 'hours').format(),
+  },
+  {
+    title: "Amazon Delivery",
+    start: moment().add(2, 'hours').format(),
     end: moment().add(3, 'hours').format(),
   },
   {
-    title: "Concert in SH",
-    start: moment().add(1, 'days').format(),
-    end: moment().add(1, 'days').add(3, 'hours').format(),
+    title: "Gym Time",
+    start: moment().add(10, 'days').add(2, 'hours').format(),
+    end: moment().add(10, 'days').add(4, 'hours').format(),
   },
   {
-    title: "Concert in HH",
-    start: moment().subtract(3, 'days').format(),
-    end: moment().subtract(3, 'days').add(3, 'hours').format(),
+    title: "Doctor's Appointment",
+    start: moment().subtract(3, 'days').add(3, 'hours').format(),
+    end: moment().subtract(3, 'days').add(4, 'hours').format(),
   },
   {
-    title: "#1 Background title",
-    start: moment().subtract(6, 'days').format('YYYY-MM-DD'),
-    end: moment().subtract(2, 'days').format('YYYY-MM-DD'),
+    title: "Piano Class",
+    start: moment().subtract(8, 'days').add(2, 'hours').format(),
+    end: moment().subtract(8, 'days').add(3, 'hours').format(),
   },
   {
-    title: "#2 title",
-    start: moment().subtract(8, 'days').format('YYYY-MM-DD'),
-    end: moment().subtract(8, 'days').format('YYYY-MM-DD'),
+    title: "Meetup w/ Terri",
+    start: moment().add(11, 'days').subtract(2, 'hours').format(),
+    end: moment().add(11, 'days').add(3, 'hours').format(),
   },
   {
-    title: "#3 Overlap",
-    start: moment().subtract(8, 'days').format('YYYY-MM-DD'),
-    end: moment().subtract(8, 'days').format('YYYY-MM-DD'),
-  }
+    title: "My Birthday!",
+    start: moment().add(19, 'days').add(2, 'hours').format(),
+    end: moment().add(19, 'days').add(5, 'hours').format(),
+  },
+  {
+    title: "Game Day",
+    start: moment().subtract(11, 'days').add(2, 'hours').format(),
+    end: moment().subtract(11, 'days').add(3, 'hours').format(),
+  },
+  {
+    title: "Cat Sitting",
+    start: moment().subtract(12, 'days').add(2, 'hours').format(),
+    end: moment().subtract(12, 'days').add(3, 'hours').format(),
+  },
+  {
+    title: "Kickback",
+    start: moment().add(16, 'days').subtract(2, 'hours').format(),
+    end: moment().add(16, 'days').add(3, 'hours').format(),
+  },
+  {
+    title: "Dentist",
+    start: moment().add(25, 'days').add(2, 'hours').format(),
+    end: moment().add(25, 'days').add(5, 'hours').format(),
+  },
+  {
+    title: "Clean House",
+    start: moment().add(20, 'days').add(2, 'hours').format(),
+    end: moment().add(20, 'days').add(3, 'hours').format(),
+  },
 ]
 
 export default sampleEvents
